faec4a8461acee88643ce0a0d2e7c24db8054c62
hedon954 <171725713@qq.com>
2024-07-17 23:20:18 +0800

    test: add test for crm-send

---
+++ crm-send/tests/service.rs
@@ -0,0 +1,51 @@
+use std::{net::SocketAddr, time::Duration};
+
+use crm_send::{
+    pb::{
+        notification_client::NotificationClient, EmailMessage, InAppMessage, SendRequest,
+        SmsMessage,
+    },
+    AppConfig, NotificationService,
+};
+use futures::StreamExt;
+use tokio::time::sleep;
+use tonic::{transport::Server, Request};
+
+#[tokio::test]
+async fn test_send() -> anyhow::Result<()> {
+    let addr = start_server().await?;
+    let mut client = NotificationClient::connect(format!("http://{}", addr)).await?;
+    let stream = tokio_stream::iter(vec![
+        SendRequest {
+            msg: Some(EmailMessage::fake().into()),
+        },
+        SendRequest {
+            msg: Some(SmsMessage::fake().into()),
+        },
+        SendRequest {
+            msg: Some(InAppMessage::fake().into()),
+        },
+    ]);
+    let req = Request::new(stream);
+    let resp = client.send(req).await?.into_inner();
+    let ret: Vec<_> = resp.then(|res| async { res.unwrap() }).collect().await;
+    assert_eq!(ret.len(), 3);
+    Ok(())
+}
+
+async fn start_server() -> anyhow::Result<SocketAddr> {
+    let config = AppConfig::load()?;
+    let addr = format!("[::1]:{}", config.server.port).parse()?;
+
+    let svc = NotificationService::new(config).into_server();
+    tokio::spawn(async move {
+        Server::builder()
+            .add_service(svc)
+            .serve(addr)
+            .await
+            .unwrap();
+    });
+    sleep(Duration::from_micros(1)).await;
+
+    Ok(addr)
+}
